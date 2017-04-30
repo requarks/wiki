f2bcc378d58cb89d6ceae1f07a361f414ff20058
NGPixel <ngpixel@gmail.com>
2017-04-30 19:41:20 -0400

    chore: Excluded docker branch from Travis build

--- client/js/pages/history.js
+++ client/js/pages/history.js
@@ -4,8 +4,6 @@ import $ from 'jquery'
 
 module.exports = (alerts) => {
   if ($('#page-type-history').length) {
-    let currentBasePath = ($('#page-type-history').data('entrypath') !== 'home') ? $('#page-type-history').data('entrypath') : ''
-
-
+    // let currentBasePath = ($('#page-type-history').data('entrypath') !== 'home') ? $('#page-type-history').data('entrypath') : ''
   }
 }
