6faf058c21a39abdeb8e4674842bf408a75b8fbc
NGPixel <github@ngpixel.com>
2017-05-26 23:01:32 -0400

    feat: updated nav buttons look

--- client/js/components/modal-create-user.vue
+++ client/js/components/modal-create-user.vue
@@ -1,37 +1,40 @@
 <template lang="pug">
-  .modal(v-bind:class='{ "is-active": isShown }')
-    .modal-background
-    .modal-container
-      .modal-content
-        header.is-blue
-          span Create / Authorize User
-          p.modal-notify(v-bind:class='{ "is-active": loading }'): i
-        section
-          label.label Email address:
-          p.control.is-fullwidth
-            input.input(type='text', placeholder='e.g. john.doe@company.com', v-model='email', autofocus)
-        section
-          label.label Provider:
-          p.control.is-fullwidth
-            select(v-model='provider')
-              option(value='local') Local Database
-              option(value='windowslive') Microsoft Account
-              option(value='google') Google ID
-              option(value='facebook') Facebook
-              option(value='github') GitHub
-              option(value='slack') Slack
-        section(v-if='provider=="local"')
-          label.label Password:
-          p.control.is-fullwidth
-            input.input(type='password', placeholder='', v-model='password')
-        section(v-if='provider=="local"')
-          label.label Full Name:
-          p.control.is-fullwidth
-            input.input(type='text', placeholder='e.g. John Doe', v-model='name')
-        footer
-          a.button.is-grey.is-outlined(v-on:click='cancel') Discard
-          a.button(v-on:click='create', v-if='provider=="local"', v-bind:disabled='loading', v-bind:class='{ "is-disabled": loading, "is-blue": !loading }') Create User
-          a.button(v-on:click='create', v-if='provider!="local"', v-bind:disabled='loading', v-bind:class='{ "is-disabled": loading, "is-blue": !loading }') Authorize User
+  transition(:duration="400")
+    .modal(v-show='isShown', v-cloak)
+      transition(name='modal-background')
+        .modal-background(v-show='isShown')
+      .modal-container
+        transition(name='modal-content')
+          .modal-content(v-show='isShown')
+            header.is-blue
+              span Create / Authorize User
+              p.modal-notify(v-bind:class='{ "is-active": loading }'): i
+            section
+              label.label Email address:
+              p.control.is-fullwidth
+                input.input(type='text', placeholder='e.g. john.doe@company.com', v-model='email', autofocus)
+            section
+              label.label Provider:
+              p.control.is-fullwidth
+                select(v-model='provider')
+                  option(value='local') Local Database
+                  option(value='windowslive') Microsoft Account
+                  option(value='google') Google ID
+                  option(value='facebook') Facebook
+                  option(value='github') GitHub
+                  option(value='slack') Slack
+            section(v-if='provider=="local"')
+              label.label Password:
+              p.control.is-fullwidth
+                input.input(type='password', placeholder='', v-model='password')
+            section(v-if='provider=="local"')
+              label.label Full Name:
+              p.control.is-fullwidth
+                input.input(type='text', placeholder='e.g. John Doe', v-model='name')
+            footer
+              a.button.is-grey.is-outlined(v-on:click='cancel') Discard
+              a.button(v-on:click='create', v-if='provider=="local"', v-bind:disabled='loading', v-bind:class='{ "is-disabled": loading, "is-blue": !loading }') Create User
+              a.button(v-on:click='create', v-if='provider!="local"', v-bind:disabled='loading', v-bind:class='{ "is-disabled": loading, "is-blue": !loading }') Authorize User
 </template>
 
 <script>
