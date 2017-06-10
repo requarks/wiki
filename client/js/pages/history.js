d590c67d50f3fc9d14dc7fd63c53df40d5abc677
NGPixel <github@ngpixel.com>
2017-06-09 20:12:27 -0400

    refactor: Removed old client page logic

--- client/js/pages/history.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import $ from 'jquery'
-
-module.exports = (alerts) => {
-  if ($('#page-type-history').length) {
-    // let currentBasePath = ($('#page-type-history').data('entrypath') !== 'home') ? $('#page-type-history').data('entrypath') : ''
-  }
-}
