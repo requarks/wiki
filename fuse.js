3d72c4ca956475742b16bcdab5b9abea5d3efd9c
NGPixel <github@ngpixel.com>
2017-07-08 20:45:29 -0400

    fix: fuse missing shims

--- fuse.js
+++ fuse.js
@@ -116,7 +116,7 @@ globalTasks.then(() => {
 
   // const bundleVendor = fuse.bundle('vendor').shim(SHIMS).instructions('~ index.js') // eslint-disable-line no-unused-vars
   // const bundleApp = fuse.bundle('app').instructions('!> [index.js]')
-  const bundleApp = fuse.bundle('app').instructions('> index.js')
+  const bundleApp = fuse.bundle('app').shim(SHIMS).instructions('> index.js')
   const bundleSetup = fuse.bundle('configure').instructions('> configure.js')
 
   switch (mode) {
