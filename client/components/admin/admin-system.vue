de2eeec506ed9994a0095592e46b94da35f47e6c
Nick <github@ngpixel.com>
2019-06-04 23:39:30 -0400

    fix: admin system translations typo

--- client/components/admin/admin-system.vue
+++ client/components/admin/admin-system.vue
@@ -91,7 +91,7 @@
                     v-list-tile-title(v-html='dbVersion')
                     v-list-tile-sub-title {{ info.dbHost }}
 
-                v-alert.mt-3(:value='isDbLimited', color='deep-orange', icon='warning') {{ $t('admin.system.dbPartialSupport') }}
+                v-alert.mt-3(:value='isDbLimited', color='deep-orange', icon='warning') {{ $t('admin:system.dbPartialSupport') }}
 </template>
 
 <script>
@@ -138,7 +138,7 @@ export default {
     async refresh() {
       await this.$apollo.queries.info.refetch()
       this.$store.commit('showNotification', {
-        message: this.$t('admin.system.refreshSuccess'),
+        message: this.$t('admin:system.refreshSuccess'),
         style: 'success',
         icon: 'cached'
       })
