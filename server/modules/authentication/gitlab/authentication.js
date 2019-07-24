7f4e392877db49320be17f90f57104edf9c0a560
Nick <github@ngpixel.com>
2019-07-23 21:21:24 -0400

    fix: gitlab baseurl typo (#934)

--- server/modules/authentication/gitlab/authentication.js
+++ server/modules/authentication/gitlab/authentication.js
@@ -14,7 +14,7 @@ module.exports = {
         clientID: conf.clientId,
         clientSecret: conf.clientSecret,
         callbackURL: conf.callbackURL,
-        baseUrl: conf.baseUrl,
+        baseURL: conf.baseUrl,
         scope: ['read_user']
       }, async (accessToken, refreshToken, profile, cb) => {
         try {
