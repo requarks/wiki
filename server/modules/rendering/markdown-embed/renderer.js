d7758bd2655a2b2023f2bcd1b369cd885cbee583
Mat Taylor <mat@GBP-iPhone-SE.WaterBit>
2020-01-02 13:38:04 -0800

    Embed plugin to support iframe block embeding of media players with extensions to support google docs, sheets, presentations and Jira JQL queries

---
+++ server/modules/rendering/markdown-embed/renderer.js
@@ -0,0 +1,82 @@
+const embed = require('markdown-it-block-embed')
+
+class PageService {
+  constructor (name, opts) {
+    this.name = name
+    this.opts = opts
+  }
+
+  extractVideoID (token) {
+    return token
+  }
+
+  getEmbedCode (token) {
+    console.log('Rendering PageService: '+ token)
+    return `<div class="embed-page" _style="border-width:0.5;border-style:dotted;width:1000px">${token}</div>`
+  }
+}
+
+class SheetService extends PageService {
+
+  getEmbedCode (token) { 
+    const path = (token.length > 45) ? `e/${token}/pubhtml?gid=0&amp;single=false&amp;widget=true` : `${token}/edit?usp=sharing&headers=off` 
+    return `
+      <div class="block-embed"><iframe 
+        src="https://docs.google.com/spreadsheets/d/${path}" frameborder=0 width=1000 height=592 allowfullscreen=true
+        mozallowfullscreen=true webkitallowfullscreen=true/>
+      </div>`
+  }
+}
+
+class GdocService extends PageService {
+
+  getEmbedCode (token) {
+    const path = (token.length > 45) ? `e/${token}/pub?embedded=true` : `${token}/edit?usp=sharing&rm=minimal` 
+    return `
+      <div class="block-embed" _style="border-width:1;border-style:solid"><iframe 
+        src="https://docs.google.com/document/d/${path}"
+        frameborder=0 width=1000 height=592 allowfullscreen=true
+        mozallowfullscreen=true webkitallowfullscreen=true/>
+      </div>`
+  }
+}
+
+class DeckService extends PageService {
+
+  getEmbedCode (token) {
+    return `
+      <div class="block-embed" _style="border-width:1;border-style:solid"><iframe 
+        src="https://docs.google.com/presentation/d/e/${token}/embed"
+        frameborder=0 width=1000 height=592 allowfullscreen=true
+        mozallowfullscreen=true webkitallowfullscreen=true/>
+      </div>`
+  }
+}
+
+class JiraService extends PageService {
+
+  getEmbedCode (token) {
+    return `
+      <base target="_top" />
+      <div class="jira-embed" style="position:relative;overflow:hidden;width:1000px;height:300px"><iframe 
+        style="position:absolute;top:-175px;height:500px;width:1000px"
+        frameborder=0
+        src="${this.opts.jiraURL}/sr/jira.issueviews:searchrequest-printable/temp/SearchRequest.html?jqlQuery=${token}"/>
+      </div>`
+  }
+}
+
+module.exports = {
+  init (md, conf) {
+    md.use(embed, {
+      jiraURL: conf.jiraURL,
+      services: {
+        page: PageService,
+        gdoc: GdocService,
+        jira: JiraService,
+        sheet: SheetService,
+        deck: DeckService
+      }
+    })
+  }
+}
