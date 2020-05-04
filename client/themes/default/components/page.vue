b8dd6d9201b9bf3e9e68b28aff775b901873af62
Reinier van der Leer <reintjebadboy@gmail.com>
2020-05-04 15:02:28 +0200

    make TOC scrollable if higher than the viewport

--- client/themes/default/components/page.vue
+++ client/themes/default/components/page.vue
@@ -56,7 +56,7 @@
       v-divider
       v-container.pl-5.pt-4(fluid, grid-list-xl)
         v-layout(row)
-          v-flex.page-col-sd(lg3, xl2, v-if='$vuetify.breakpoint.lgAndUp', align-self-start, style='margin-top: -90px; position: sticky; top: 70px;')
+          v-flex.page-col-sd(lg3, xl2, v-if='$vuetify.breakpoint.lgAndUp')
             v-card.mb-5(v-if='toc.length')
               .overline.pa-5.pb-0(:class='darkMode ? `blue--text text--lighten-2` : `primary--text`') {{$t('common:page.toc')}}
               v-list.pb-3(dense, nav, :class='darkMode ? `darken-3-d3` : ``')
@@ -529,6 +529,20 @@ export default {
   .v-breadcrumbs__divider:nth-child(2) {
     padding: 0 6px 0 12px;
   }
+}
+
+.page-col-sd {
+  margin-top: -90px;
+  align-self: flex-start;
+  position: sticky;
+  top: 64px;
+  max-height: calc(100vh - 64px);
+  overflow-y: auto;
+  -ms-overflow-style: none;
+}
+
+.page-col-sd::-webkit-scrollbar {
+  display: none;
 }
 
 </style>
