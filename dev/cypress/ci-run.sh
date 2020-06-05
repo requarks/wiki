4358e7939c613b6752adaa374eed70b8186ce1a4
NGPixel <github@ngpixel.com>
2020-06-04 20:56:47 -0400

    fix: cypress ci-run.sh dir path

--- dev/cypress/ci-run.sh
+++ dev/cypress/ci-run.sh
@@ -1,1 +1,3 @@
-docker run --name cypress -v $(Build.SourcesDirectory):/e2e -w /e2e -e "CYPRESS_RECORD_KEY=$(CYPRESS_KEY)" cypress/included:4.7.0 --record --headless --ci-build-id "$(Build.BuildNumber)" --config baseUrl=http://host.docker.internal:3000
+echo "$(Build.SourcesDirectory)"
+echo $BUILD_SOURCESDIRECTORY
+docker run --name cypress -v $BUILD_SOURCESDIRECTORY:/e2e -w /e2e -e "CYPRESS_RECORD_KEY=$(CYPRESS_KEY)" cypress/included:4.7.0 --record --headless --ci-build-id "$(Build.BuildNumber)" --config baseUrl=http://host.docker.internal:3000
