6d1cecabd4e639057b1470cfa20cd49500dea71e
NGPixel <github@ngpixel.com>
2020-06-04 20:59:38 -0400

    fix: cypress ci-run vars

--- dev/cypress/ci-run.sh
+++ dev/cypress/ci-run.sh
@@ -1,3 +1,2 @@
-echo "$(Build.SourcesDirectory)"
 echo $BUILD_SOURCESDIRECTORY
-docker run --name cypress -v $BUILD_SOURCESDIRECTORY:/e2e -w /e2e -e "CYPRESS_RECORD_KEY=$(CYPRESS_KEY)" cypress/included:4.7.0 --record --headless --ci-build-id "$(Build.BuildNumber)" --config baseUrl=http://host.docker.internal:3000
+docker run --name cypress -v $BUILD_SOURCESDIRECTORY:/e2e -w /e2e -e "CYPRESS_RECORD_KEY=$CYPRESS_KEY" cypress/included:4.7.0 --record --headless --ci-build-id "$BUILD_BUILDNUMBER" --config baseUrl=http://172.17.0.1:3000
