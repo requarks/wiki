9dd85fba1a70f8ef57c97a446c14ac68bb8d3853
NGPixel <github@ngpixel.com>
2020-06-04 21:18:52 -0400

    fix: remove cypress run file

--- dev/cypress/ci-run.sh
+++
@@ -1,2 +0,0 @@
-echo $BUILD_SOURCESDIRECTORY
-docker run --name cypress -v $BUILD_SOURCESDIRECTORY:/e2e -w /e2e cypress/included:4.7.0 --record --key "$CYPRESS_KEY" --headless --group "$TEST_MATRIX" --ci-build-id "$BUILD_BUILDNUMBER" --config baseUrl=http://172.17.0.1:3000
