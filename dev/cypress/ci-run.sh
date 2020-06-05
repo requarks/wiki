4376e70fce911d5cca0fd1bcd616f2eb06ab96bb
NGPixel <github@ngpixel.com>
2020-06-04 21:13:16 -0400

    fix: cypress record key

--- dev/cypress/ci-run.sh
+++ dev/cypress/ci-run.sh
@@ -1,2 +1,2 @@
 echo $BUILD_SOURCESDIRECTORY
-docker run --name cypress -v $BUILD_SOURCESDIRECTORY:/e2e -w /e2e -e "CYPRESS_RECORD_KEY=$CYPRESS_KEY" cypress/included:4.7.0 --record --headless --group "$TEST_MATRIX" --ci-build-id "$BUILD_BUILDNUMBER" --config baseUrl=http://172.17.0.1:3000
+docker run --name cypress -v $BUILD_SOURCESDIRECTORY:/e2e -w /e2e cypress/included:4.7.0 --record --key "$CYPRESS_KEY" --headless --group "$TEST_MATRIX" --ci-build-id "$BUILD_BUILDNUMBER" --config baseUrl=http://172.17.0.1:3000
