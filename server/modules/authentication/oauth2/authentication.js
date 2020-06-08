c1e0de73bd35888e947c6a8ab2a16183fa4ff542
Vitaly Ketsenko <v.ketsenko@gmail.com>
2020-06-08 17:10:12 +0300

    oauth2: enabled module, parse user profile from jwt

--- server/modules/authentication/oauth2/authentication.js
+++ server/modules/authentication/oauth2/authentication.js
@@ -4,24 +4,27 @@
 // OAuth2 Account
 // ------------------------------------
 
+const jwtParser = require('jsonwebtoken');
 const OAuth2Strategy = require('passport-oauth2').Strategy
 
 module.exports = {
   init (passport, conf) {
-    passport.use('oauth2',
-      new OAuth2Strategy({
+	var client = new OAuth2Strategy({
         authorizationURL: conf.authorizationURL,
         tokenURL: conf.tokenURL,
         clientID: conf.clientId,
         clientSecret: conf.clientSecret,
         callbackURL: conf.callbackURL
       }, (accessToken, refreshToken, profile, cb) => {
-        WIKI.models.users.processProfile(profile).then((user) => {
+        WIKI.models.users.processProfile({ profile, providerKey: 'oauth2' }).then((user) => {
           return cb(null, user) || true
         }).catch((err) => {
           return cb(err, null) || true
         })
-      })
-    )
+      });
+	client.userProfile = (accessToken, done) => {
+			done(null, jwtParser.decode(accessToken));
+		};
+    passport.use('oauth2', client);
   }
 }
