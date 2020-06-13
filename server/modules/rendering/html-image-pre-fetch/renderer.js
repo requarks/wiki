f725d65193bbcc1ef7818e268b848a39002e5c65
regevbr <regevbr@gmail.com>
2020-06-13 23:55:21 +0300

    feat: Caching kroki svgs #2020

--- server/modules/rendering/html-image-pre-fetch/renderer.js
+++ server/modules/rendering/html-image-pre-fetch/renderer.js
@@ -1,21 +1,19 @@
 const request = require('request-promise')
-const _ = require('lodash')
 
 module.exports = {
   async init($) {
     const promises = [];
     const handler = async (elm) => {
-      elm.removeClass('pre-fetch-candidate');
       const url = elm.attr(`src`)
       const response = await request({
         method: `GET`,
         url,
         resolveWithFullResponse: true
       })
-      const image = response.body
       const contentType = response.headers[`content-type`]
-      const base64 = Buffer.from(image).toString('base64')
-      elm.attr('src', `data:${contentType};base64,${base64}`)
+      const image = Buffer.from(response.body).toString('base64')
+      elm.attr('src', `data:${contentType};base64,${image}`)
+      elm.removeClass('pre-fetch-candidate');
     }
     $('img.pre-fetch-candidate').each((i, elm) => {
       promises.push(handler($(elm)))
