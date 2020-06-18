89fe88df4eacd6b829f10a99691462c518e00818
regevbr <regevbr@gmail.com>
2020-06-19 01:14:28 +0300

    feat: Caching kroki svgs #2020

--- server/modules/rendering/html-image-prefetch/renderer.js
+++ server/modules/rendering/html-image-prefetch/renderer.js
@@ -2,11 +2,18 @@ const request = require('request-promise')
 
 const prefetch = async (element) => {
   const url = element.attr(`src`)
-  const response = await request({
-    method: `GET`,
-    url,
-    resolveWithFullResponse: true
-  })
+  let response
+  try {
+    response = await request({
+      method: `GET`,
+      url,
+      resolveWithFullResponse: true
+    })
+  } catch (err) {
+    WIKI.logger.warn(`Failed to prefetch ${url}`)
+    WIKI.logger.warn(err)
+    return
+  }
   const contentType = response.headers[`content-type`]
   const image = Buffer.from(response.body).toString('base64')
   element.attr('src', `data:${contentType};base64,${image}`)
@@ -15,7 +22,7 @@ const prefetch = async (element) => {
 
 module.exports = {
   async init($) {
-    const promises = $('img.prefetch-candidate').map((_, element) => {
+    const promises = $('img.prefetch-candidate').map((index, element) => {
       return prefetch($(element))
     }).toArray()
     await Promise.all(promises)
