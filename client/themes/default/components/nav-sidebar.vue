9e4ed7986c2103e94e9bf5005af8c7f570cfbc7f
Mauro Frischherz <fmauro@users.noreply.github.com>
2020-06-19 04:05:41 +0200

    fix: include locale in sidebar browse links (#1807)
    Co-authored-by: Mauro Frischherz <mauro.frischherz@eoscop.ch>

--- client/themes/default/components/nav-sidebar.vue
+++ client/themes/default/components/nav-sidebar.vue
@@ -43,7 +43,7 @@
           v-list-item-avatar(size='24')
             v-icon mdi-folder
           v-list-item-title {{ item.title }}
-        v-list-item(v-else, :href='`/` + item.path', :key='`childpage-` + item.id', :input-value='path === item.path')
+        v-list-item(v-else, :href='`/` + item.locale + `/` + item.path', :key='`childpage-` + item.id', :input-value='path === item.path')
           v-list-item-avatar(size='24')
             v-icon mdi-text-box
           v-list-item-title {{ item.title }}
@@ -135,6 +135,7 @@ export default {
                 isFolder
                 pageId
                 parent
+                locale
               }
             }
           }
@@ -162,6 +163,7 @@ export default {
                 isFolder
                 pageId
                 parent
+                locale
               }
             }
           }
