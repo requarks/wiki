af054257bd0c1bd8f5b6685450d54fde4b7b37ec
NGPixel <github@ngpixel.com>
2020-09-06 15:36:51 -0400

    fix: 2.5.108 migration (2)

--- server/db/migrations/2.5.108.js
+++ server/db/migrations/2.5.108.js
@@ -2,7 +2,7 @@ const has = require('lodash/has')
 
 exports.up = async knex => {
   // -> Fix 2.5.1 added isEnabled columns for beta users
-  const localStrategy = await knex('authentication').where('key', 'local')
+  const localStrategy = await knex('authentication').where('key', 'local').first()
   if (localStrategy && !has(localStrategy, 'isEnabled')) {
     await knex.schema
       .alterTable('authentication', table => {
