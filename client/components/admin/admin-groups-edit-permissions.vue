02c6e1f7bcf0f70d91a9453865bf5d734db1790d
Rainshaw <rxg@live.com>
2020-09-16 23:59:03 +0800

    fix: admin groups permission value

--- client/components/admin/admin-groups-edit-permissions.vue
+++ client/components/admin/admin-groups-edit-permissions.vue
@@ -23,7 +23,7 @@
                   persistent-hint
                   color='primary'
                   v-model='group.permissions'
-                  :value='pm.permission'
+                  :value='pm.value'
                   :append-icon='pm.warning ? "mdi-alert" : null',
                   :disabled='(group.isSystem && pm.restrictedForSystem) || group.id === 1 || pm.disabled'
                 )
@@ -45,6 +45,7 @@ export default {
           category: this.$t('admin:permission.contentTitle'),
           items: [
             {
+              value: 'read:pages',
               permission: this.$t('admin:permission.readPages'),
               hint: this.$t('admin:permission.readPagesHint'),
               warning: false,
@@ -52,6 +53,7 @@ export default {
               disabled: false
             },
             {
+              value: 'write:pages',
               permission: this.$t('admin:permission.writePages'),
               hint: this.$t('admin:permission.writePagesHint'),
               warning: false,
@@ -59,6 +61,7 @@ export default {
               disabled: false
             },
             {
+              value: 'manage:pages',
               permission: this.$t('admin:permission.managePages'),
               hint: this.$t('admin:permission.managePagesHint'),
               warning: false,
@@ -66,6 +69,7 @@ export default {
               disabled: false
             },
             {
+              value: 'delete:pages',
               permission: this.$t('admin:permission.deletePages'),
               hint: this.$t('admin:permission.deletePagesHint'),
               warning: false,
@@ -73,6 +77,7 @@ export default {
               disabled: false
             },
             {
+              value: 'write:styles',
               permission: this.$t('admin:permission.writeStyles'),
               hint: this.$t('admin:permission.writeStylesHint'),
               warning: false,
@@ -80,6 +85,7 @@ export default {
               disabled: false
             },
             {
+              value: 'write:scripts',
               permission: this.$t('admin:permission.writeScripts'),
               hint: this.$t('admin:permission.writeScriptsHint'),
               warning: false,
@@ -87,6 +93,7 @@ export default {
               disabled: false
             },
             {
+              value: 'read:source',
               permission: this.$t('admin:permission.readSource'),
               hint: this.$t('admin:permission.readSourceHint'),
               warning: false,
@@ -94,6 +101,7 @@ export default {
               disabled: false
             },
             {
+              value: 'read:history',
               permission: this.$t('admin:permission.readHistory'),
               hint: this.$t('admin:permission.readHistoryHint'),
               warning: false,
@@ -101,6 +109,7 @@ export default {
               disabled: false
             },
             {
+              value: 'read:assets',
               permission: this.$t('admin:permission.readAssets'),
               hint: this.$t('admin:permission.readAssetsHint'),
               warning: false,
@@ -108,6 +117,7 @@ export default {
               disabled: false
             },
             {
+              value: 'write:assets',
               permission: this.$t('admin:permission.writeAssets'),
               hint: this.$t('admin:permission.writeAssetsHint'),
               warning: false,
@@ -115,6 +125,7 @@ export default {
               disabled: false
             },
             {
+              value: 'maange:assets',
               permission: this.$t('admin:permission.manageAssets'),
               hint: this.$t('admin:permission.manageAssetsHint'),
               warning: false,
@@ -122,6 +133,7 @@ export default {
               disabled: false
             },
             {
+              value: 'read:comments',
               permission: this.$t('admin:permission.readComments'),
               hint: this.$t('admin:permission.readCommentsHint'),
               warning: false,
@@ -129,6 +141,7 @@ export default {
               disabled: false
             },
             {
+              value: 'write:comments',
               permission: this.$t('admin:permission.writeComments'),
               hint: this.$t('admin:permission.writeCommentsHint'),
               warning: false,
@@ -136,6 +149,7 @@ export default {
               disabled: false
             },
             {
+              value: 'manage:comments',
               permission: this.$t('admin:permission.manageComments'),
               hint: this.$t('admin:permission.manageCommentsHint'),
               warning: false,
@@ -148,6 +162,7 @@ export default {
           category: this.$t('admin:permission.usersTitle'),
           items: [
             {
+              value: 'write:users',
               permission: this.$t('admin:permission.writeUsers'),
               hint: this.$t('admin:permission.writeUsersHint'),
               warning: false,
@@ -155,6 +170,7 @@ export default {
               disabled: false
             },
             {
+              value: 'manage:users',
               permission: this.$t('admin:permission.manageUsers'),
               hint: this.$t('admin:permission.manageUsersHint'),
               warning: false,
@@ -162,6 +178,7 @@ export default {
               disabled: false
             },
             {
+              value: 'write:groups',
               permission: this.$t('admin:permission.writeGroups'),
               hint: this.$t('admin:permission.writeGroupsHint'),
               warning: false,
@@ -169,6 +186,7 @@ export default {
               disabled: false
             },
             {
+              value: 'manage:groups',
               permission: this.$t('admin:permission.manageGroups'),
               hint: this.$t('admin:permission.manageGroupsHint'),
               warning: true,
@@ -181,6 +199,7 @@ export default {
           category: this.$t('admin:permission.administrationTitle'),
           items: [
             {
+              value: 'manage:navigation',
               permission: this.$t('admin:permission.manageNavigation'),
               hint: this.$t('admin:permission.manageNavigationHint'),
               warning: false,
@@ -188,6 +207,7 @@ export default {
               disabled: false
             },
             {
+              value: 'manage:theme',
               permission: this.$t('admin:permission.manageTheme'),
               hint: this.$t('admin:permission.manageThemeHint'),
               warning: false,
@@ -195,6 +215,7 @@ export default {
               disabled: false
             },
             {
+              value: 'manage:api',
               permission: this.$t('admin:permission.manageApi'),
               hint: this.$t('admin:permission.manageApiHint'),
               warning: true,
@@ -202,6 +223,7 @@ export default {
               disabled: false
             },
             {
+              value: 'manage:system',
               permission: this.$t('admin:permission.manageSystem'),
               hint: this.$t('admin:permission.manageSystemHint'),
               warning: true,
