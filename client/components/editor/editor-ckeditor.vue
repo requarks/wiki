0943c6098d1edc7aad17fdfbf4a2d075bea27314
Andrew Hughes <andrewcarterhughes@gmail.com>
2020-11-22 20:28:01 -0800

    updated to use local ckeditor dependency

--- client/components/editor/editor-ckeditor.vue
+++ client/components/editor/editor-ckeditor.vue
@@ -17,8 +17,8 @@
 <script>
 import _ from 'lodash'
 import { get, sync } from 'vuex-pathify'
-import DecoupledEditor from '@requarks/ckeditor5'
-// import DecoupledEditor from '../../../../wiki-ckeditor5/build/ckeditor'
+//import DecoupledEditor from '@requarks/ckeditor5'
+import DecoupledEditor from '../../../../wiki-ckeditor5'
 import EditorConflict from './ckeditor/conflict.vue'
 import { html as beautify } from 'js-beautify/js/lib/beautifier.min.js'
 
