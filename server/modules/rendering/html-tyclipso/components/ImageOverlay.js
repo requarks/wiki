eae23957d63eb2641f9293f9840195784772cdf7
Carl Richter <carl.richter@tyclipso.net>
2021-07-30 13:18:18 +0200

    feat: add styling for dark theme

--- server/modules/rendering/html-tyclipso/components/ImageOverlay.js
+++ server/modules/rendering/html-tyclipso/components/ImageOverlay.js
@@ -5,9 +5,9 @@
 function ImageOverlay(src) {
   return `
     <div class="ty-image-overlay">
-      <a href="${src}" target="_blank" class="v-btn v-btn--contained theme--light v-size--default primary" aria-label="In neuem Tab öffnen">
+      <a href="${src}" target="_blank" class="v-btn v-btn--contained v-size--default primary" aria-label="In neuem Tab öffnen">
         <span class="v-btn__content">
-            <i aria-hidden="true" class="v-icon v-icon--left notranslate mdi mdi-open-in-new theme--light"></i>
+            <i aria-hidden="true" class="v-icon v-icon--left notranslate mdi mdi-open-in-new"></i>
             In neuem Tab öffnen
         </span>
       </a>
