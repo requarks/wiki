d5a5820c2f59e1315b5ba225809167f855a0131f
NGPixel <github@ngpixel.com>
2021-09-11 21:02:53 -0400

    fix: remove unused middleware

--- server/middlewares/upload.js
+++
@@ -1,6 +0,0 @@
-const { graphqlUploadExpress } = require('graphql-upload')
-
-/**
- * GraphQL File Upload Middleware
- */
-module.exports = graphqlUploadExpress({ maxFileSize: 5000000, maxFiles: 20 })
