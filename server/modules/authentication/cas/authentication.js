9f6f226832cd03c10f2e1ee3119e765b78075828
SeaLife <mtries@united-internet.de>
2022-07-07 15:44:32 +0200

    [F.34] Added basic cas implementation, tested locally

--- server/modules/authentication/cas/authentication.js
+++ server/modules/authentication/cas/authentication.js
@@ -10,14 +10,33 @@ module.exports = {
   init (passport, conf) {
     passport.use(conf.key,
       new CASStrategy({
+        version: 'CAS3.0',
         ssoBaseURL: conf.ssoBaseURL,
         serverBaseURL: conf.serverBaseURL,
         passReqToCallback: true
       }, async (req, profile, cb) => {
         try {
+          let userEmail = null
+          let displayName = null
+
+          if (conf.emailAttribute) {
+            userEmail = profile.attributes[ conf.emailAttribute ]
+          }
+
+          if (conf.displayNameAttribute) {
+            displayName = profile.attributes[ conf.displayNameAttribute ]
+          } else {
+            displayName = profile.user
+          }
+
           const user = await WIKI.models.users.processProfile({
             providerKey: req.params.strategy,
-            profile
+            profile: {
+              ...profile,
+              id: profile.user,
+              email: userEmail,
+              name: displayName
+            }
           })
           cb(null, user)
         } catch (err) {
