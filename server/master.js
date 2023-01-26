7a7c9a7fe3cf0b6e8920f18a602ca23c170bdef9
Aviv Haim <avivos40@gmail.com>
2023-01-26 16:28:37 +0200

    Allow cross origin

--- server/master.js
+++ server/master.js
@@ -42,8 +42,8 @@ module.exports = async () => {
   // ----------------------------------------
 
   app.use(mw.security)
-  app.use(cors({ origin: false }))
-  app.options('*', cors({ origin: false }))
+  app.use(cors({ origin: true }))
+  app.options('*', cors({ origin: true }))
   if (WIKI.config.security.securityTrustProxy) {
     app.enable('trust proxy')
   }
