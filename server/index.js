e1d282ad1102fb298528238d3c6f18f073dbeb7d
Nicolas Giard <github@ngpixel.com>
2023-05-11 21:10:15 -0400

    feat: warn and exit on unsupported node version

--- server/index.js
+++ server/index.js
@@ -6,6 +6,19 @@
 const path = require('path')
 const { nanoid } = require('nanoid')
 const { DateTime } = require('luxon')
+const { gte } = require('semver')
+
+// ----------------------------------------
+// Check Node.js version
+// ----------------------------------------
+if (gte(process.version, '18.0.0')) {
+  console.error('You\'re using an unsupported Node.js version. Please read the requirements.')
+  process.exit(1)
+}
+
+// ----------------------------------------
+// Init WIKI instance
+// ----------------------------------------
 
 let WIKI = {
   IS_DEBUG: process.env.NODE_ENV === 'development',
