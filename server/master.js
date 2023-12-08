d1eb3777b83af1c5bfdb179b92178f5563635f6d
root <root@wiki-silicowavesintern.ojnzxon1u4zupapcrdl2vnyvje.bx.internal.cloudapp.net>
2023-12-08 05:38:36 +0000

    updated cros error

--- server/master.js
+++ server/master.js
@@ -42,8 +42,8 @@ module.exports = async () => {
   // ----------------------------------------
 
   app.use(mw.security)
-  app.use(cors({ origin: false }))
-  app.options('*', cors({ origin: false }))
+  app.use(cors())
+  app.options('*', cors())
   if (WIKI.config.security.securityTrustProxy) {
     app.enable('trust proxy')
   }
