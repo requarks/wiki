abd4c099740fc6349b6255fc7b07c85d08731db7
guus.damen <guus.damen@ptlsbw.onmicrosoft.com>
2024-07-15 16:15:03 +0200

    Removed incorrect files

--- deployment/run-backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SERVICE=wiki SERVICE_DB=wiki-db DB_USER=postgres DATABASE=wiki ./backup.sh --dev1
-
-SERVICE=keycloak SERVICE_DB=keycloak-db DB_USER=postgres DATABASE=keycloak ./backup.sh --dev1
