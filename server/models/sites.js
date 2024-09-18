720bfcf7de8b0296df6336312fee7f31eadc137c
Michal Tuleja <michal.tuleja@capgemini.com>
2024-09-18 18:16:56 +0200

    Add site models

---
+++ server/models/sites.js
@@ -0,0 +1,69 @@
+const Model = require('objection').Model
+const { keyBy } = require('lodash')
+
+/**
+ * Site model
+ */
+module.exports = class Site extends Model {
+  static get tableName() { return 'sites' }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['path'],
+
+      properties: {
+        id: { type: 'string' },
+        name: { type: 'string' },
+        path: { type: 'string' },
+        isEnabled: { type: 'boolean', default: false }
+      }
+    }
+  }
+
+  static get jsonAttributes() {
+    return ['config']
+  }
+
+  static async getSiteByPath({ path, forceReload = false }) {
+    if (forceReload) {
+      await WIKI.db.sites.reloadCache()
+    }
+    const siteId = WIKI.sitesMappings[path] || WIKI.sitesMappings['first_site']
+    if (siteId) {
+      return WIKI.sites[siteId]
+    }
+    return null
+  }
+
+  static async reloadCache() {
+    WIKI.logger.info('Reloading site configurations...')
+    const sites = await WIKI.db.sites.query().orderBy('id')
+    WIKI.sites = keyBy(sites, 'id')
+    WIKI.sitesMappings = {}
+    for (const site of sites) {
+      WIKI.sitesMappings[site.path] = site.id
+    }
+    WIKI.logger.info(`Loaded ${sites.length} site configurations [ OK ]`)
+  }
+
+  static async createSite(name, path) {
+    const newSite = await WIKI.db.sites.query().insertAndFetch({
+      name,
+      isEnabled: true,
+      path,
+      config: {}
+    });
+
+    return newSite
+  }
+
+  static async updateSite(id, patch) {
+    return WIKI.db.sites.query().findById(id).patch(patch)
+  }
+
+  static async deleteSite(id) {
+    await WIKI.db.storage.query().delete().where('siteId', id)
+    return WIKI.db.sites.query().deleteById(id)
+  }
+}
