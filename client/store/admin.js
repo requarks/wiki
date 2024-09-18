5b796a02a1e85e238be6d2638bd5df2938ce5ecc
Yannik Selbach <yannik.selbach@capgemini.com>
2024-09-18 08:46:30 +0200

    MAR-171: Page Rules Tab: added temporary graphql query for fechting all sites

--- client/store/admin.js
+++ client/store/admin.js
@@ -1,4 +1,6 @@
 import { make } from 'vuex-pathify'
+import { gql } from "@apollo/client/core";
+import pagesQuery from 'gql/admin/pages/pages-query-list.gql'
 
 const state = {
   info: {
@@ -7,11 +9,38 @@ const state = {
     groupsTotal: 0,
     pagesTotal: 0,
     usersTotal: 0
-  }
+  },
+  sites: []
 }
 
 export default {
   namespaced: true,
   state,
-  mutations: make.mutations(state)
-}
+  mutations: {
+    ...make.mutations(state),
+  SET_SITES(state, sites) {
+    state.sites = sites;
+  },
+},
+
+  actions: {
+      async fetchSites({ commit }, { apolloClient }) {
+        try {
+          const response = await apolloClient.query({
+            query: pagesQuery,
+            fetchPolicy: 'network-only'
+          });
+  
+          console.log(response.data);
+          console.log("Fetched Sites:", response.data.pages.list); 
+          const sites = response.data.pages.list;
+  
+          commit("SET_SITES", sites);
+          return response;
+        } catch (error) {
+          console.error("Error fetching sites:", error);
+          throw error;
+        }
+      },
+  },
+};
