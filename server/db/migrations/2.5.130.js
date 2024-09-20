5e6cb226543b7a12f43ae14a96bd03d174c46a0f
Michal Tuleja <michal.tuleja@capgemini.com>
2024-09-20 14:36:08 +0200

    Add site relation with other tables

---
+++ server/db/migrations/2.5.130.js
@@ -0,0 +1,29 @@
+/* global WIKI */
+
+exports.up = async knex => {
+  // TODO: SiteId cannot be null at the inception.
+  //       Consider adding a default site and refer it here with an UPDATE ... SET ... IF NOT NULL
+
+  return knex.schema
+    .table('assetFolders', table => {
+      table.uuid('siteId').references('id').inTable('sites').index()
+    })
+    .table('assets', table => {
+      table.uuid('siteId').references('id').inTable('sites').index()
+    })
+    .table('pageHistory', table => {
+      table.uuid('siteId').references('id').inTable('sites').index()
+    })
+    .table('pages', table => {
+      table.uuid('siteId').references('id').inTable('sites').index() // .alter()
+    })
+    .table('tags', table => {
+      table.uuid('siteId').references('id').inTable('sites')
+      table.unique(['siteId', 'tag'])
+    })
+    .table('pageTree', table => {
+      table.uuid('siteId').references('id').inTable('sites').index()
+    })
+}
+
+exports.down = knex => { }
