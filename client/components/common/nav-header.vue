04fda566e8d524ae6a235593bfe1996cc624f1fa
Michal Tuleja <michal.tuleja@capgemini.com>
2024-10-17 09:51:31 +0200

    MAR-178: Add shadow under the dropdown

--- client/components/common/nav-header.vue
+++ client/components/common/nav-header.vue
@@ -43,7 +43,7 @@
                     )
                     span Sites
             v-list(
-                style="overflow-y: auto;"
+                style="overflow-y: auto; box-shadow: 0 3px 5px -1px rgba(0, 0, 0, .2); 0 6px 10px 0 rgba(0, 0, 0, .14); 0 1px 18px 0 rgba(0, 0, 0, .12);"
                 width='168'
                 nav, :light='!$vuetify.theme.dark'
                 :dark='$vuetify.theme.dark'
