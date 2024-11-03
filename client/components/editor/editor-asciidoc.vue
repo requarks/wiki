378481e14fbe3509f80039470a908a1d943c4818
Brian Powell Jr <b.powell.jr@outlook.com>
2024-11-03 11:00:12 -0500

    Changed SUCCESS blockquote adoc option to return a block as AsciiDoctor.js currently doesn't support inline extensions on paragraph content

--- client/components/editor/editor-asciidoc.vue
+++ client/components/editor/editor-asciidoc.vue
@@ -53,7 +53,7 @@
                 v-icon(color='blue') mdi-alpha-n-box-outline
               v-list-item-title {{'Note Blockquote'}}
             v-divider
-            v-list-item(@click='insertBeforeEachLine({ content: `SUCCESS: `})')
+            v-list-item(@click='insertBeforeEachLine({ content: `[SUCCESS]\n`})')
               v-list-item-action
                 v-icon(color='success') mdi-alpha-s-box-outline
               v-list-item-title {{'Success Blockquote'}}
