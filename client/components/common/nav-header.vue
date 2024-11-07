24fb13eefe9c3267d733c1736baffe11d29fcf10
Yannik Selbach <yannik.selbach@capgemini.com>
2024-11-07 11:41:51 +0100

    add dropdown arrow to sites dropdown

--- client/components/common/nav-header.vue
+++ client/components/common/nav-header.vue
@@ -42,6 +42,7 @@
                     style="overflow: none;"
                     )
                     span Sites
+                    v-icon(color='grey') mdi-chevron-down
             v-list(
                 style="overflow-y: auto; box-shadow: 0 3px 5px -1px rgba(0, 0, 0, .2); 0 6px 10px 0 rgba(0, 0, 0, .14); 0 1px 18px 0 rgba(0, 0, 0, .12);"
                 width='168'
