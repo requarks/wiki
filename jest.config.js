c4b38560398eacffc5c49d34fd14785ace4800d4
Hari Kumar Gurudu <hari-kumar.gurudu@capgemini.com>
2025-01-07 14:19:54 +0000

    Feature/mar 390 cicd pipeline build test

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,17 @@ const config = {
   testPathIgnorePatterns: [
     "/node_modules/",
     "/dev\/cypress\/"
-  ]
+  ],
+  "reporters": [
+      [
+        "jest-junit",
+        {
+          "outputDirectory": "./test-results",
+          "outputName": "report.xml",
+          "addFileAttribute": "true"
+        }
+      ]
+   ]
 };
 
 module.exports = config;
