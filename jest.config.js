025a40a5dda51c2d2ed7e93aec3d6a254e518411
Michal Tuleja <michal.tuleja@capgemini.com>
2025-01-15 15:59:10 +0100

    MAR-477: Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -6,16 +6,6 @@ const config = {
     '/node_modules/',
     "/dev\/cypress\/"
   ],
-  // "reporters": [
-  //     [
-  //       "jest-junit",
-  //       {
-  //         "outputDirectory": "./test-results",
-  //         "outputName": "report.xml",
-  //         "addFileAttribute": "true"
-  //       }
-  //     ]
-  // ],
   rootDir: './',
   testMatch: ['**/server/test/**/*.test.js']
 }
