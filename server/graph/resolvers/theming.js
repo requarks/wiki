aaba6d19fa8952d29e29f60128c8d210153da7cb
Nils Urmersbach <nils.urmersbach@capgemini.com>
2025-02-11 15:56:48 +0100

    MAR-577: Changing the default table of content position to the right (UI improvements)

--- server/graph/resolvers/theming.js
+++ server/graph/resolvers/theming.js
@@ -24,7 +24,7 @@ module.exports = {
         theme: WIKI.config.theming.theme,
         iconset: WIKI.config.theming.iconset,
         darkMode: WIKI.config.theming.darkMode,
-        tocPosition: WIKI.config.theming.tocPosition || 'left',
+        tocPosition: WIKI.config.theming.tocPosition || 'right',
         injectCSS: new CleanCSS({ format: 'beautify' }).minify(WIKI.config.theming.injectCSS).styles,
         injectHead: WIKI.config.theming.injectHead,
         injectBody: WIKI.config.theming.injectBody
@@ -45,7 +45,7 @@ module.exports = {
           theme: args.theme,
           iconset: args.iconset,
           darkMode: args.darkMode,
-          tocPosition: args.tocPosition || 'left',
+          tocPosition: args.tocPosition || 'right',
           injectCSS: args.injectCSS || '',
           injectHead: args.injectHead || '',
           injectBody: args.injectBody || ''
