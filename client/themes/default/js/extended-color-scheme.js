a597f890d0d1170cd03c879a441dbb467c63795a
Nils Urmersbach <nils.urmersbach@capgemini.com>
2025-02-18 14:15:12 +0100

    MAR-616: Adding a mirror of the extended material colors to be used with vuetify components

---
+++ client/themes/default/js/extended-color-scheme.js
@@ -0,0 +1,102 @@
+/*
+ * This object is a mirror of the extended material colors of client/scss/base/material.scss
+ */
+export const colors = {
+  'green': {
+    '1': '#57CF80',
+    '2': '#33B569',
+    '3': '#2EA657',
+    '4': '#178C3D',
+    '5': '#178036',
+  },
+
+  'teal': {
+    '1': '#00E6E3',
+    '2': '#00D5D0',
+    '3': '#00BFBF',
+    '4': '#00929B',
+    '5': '#007D74',
+  },
+
+  'peacock': {
+    '1': '#00E0CB',
+    '2': '#00B2A2',
+    '3': '#0F878A',
+    '4': '#0F6A73',
+    '5': '#0F434A',
+  },
+
+  'sapphire': {
+    '1': '#338091',
+    '2': '#336B7D',
+    '3': '#14596B',
+    '4': '#214554',
+    '5': '#173340',
+  },
+
+  'violet': {
+    '1': '#E557AD',
+    '2': '#D13A8C',
+    '3': '#BA2980',
+    '4': '#A12980',
+    '5': '#811B6F',
+  },
+
+  'yellow': {
+    '1': '#FFDA80',
+    '2': '#FFD068',
+    '3': '#FFB24A',
+    '4': '#FF9C29',
+    '5': '#FF8E12',
+  },
+
+  'velvet': {
+    '1': '#9E4780',
+    '2': '#802B73',
+    '3': '#750D5C',
+    '4': '#590A42',
+    '5': '#42142E',
+  },
+
+  'red': {
+    '1': '#FF5770',
+    '2': '#FF455E',
+    '3': '#FF304D',
+    '4': '#E30021',
+    '5': '#A6001A',
+  },
+
+  'primary': {
+    '1': '#0070AD',
+    '2': '#12ABDB',
+    '3': '#2B0A3D',
+  },
+
+  'surface': {
+    '1': '#F2F2F2',
+    '2': '#ECECEC',
+    '3': '#CCCCCC',
+  },
+
+  'text': {
+    'dark-purple': '#15011D',
+    'dark-grey': '#272936',
+  },
+
+  'alert': {
+    'error': '#E30021',
+    'errorOnDark': '#FF455E',
+    'errorLight': '#FFEFEF',
+    'info': '#0070AD',
+    'infoOnDark': '#12ABDB',
+    'infoLight': '#ECF3FF',
+    'success': '#2EA657',
+    'successOnDark': '#33B569',
+    'successLight': '#E5FCEB',
+    'warning': '#FF8E12',
+    'warningOnDark': '#FF8E12',
+    'warningLight': '#FFF7DE',
+  },
+
+  'white': '#FFFFFF',
+};
