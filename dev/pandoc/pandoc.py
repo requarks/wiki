831b2a5b7a3be06d2436d2cd5d79e2ad5881c087
Nils Urmersbach <nils.urmersbach@capgemini.com>
2025-03-12 09:25:52 +0100

    MAR-607: Adding export logic lost after merge conflict

--- dev/pandoc/pandoc.py
+++ dev/pandoc/pandoc.py
@@ -4,12 +4,13 @@ import subprocess
 app = Flask(__name__)
 
 @app.route('/convert-to-docx', methods=['POST'])
-def convert():
+def convert_to_docx():
     try:
       input_file = request.files['file']
       input_file.save('/app/input.html')
       output_file = '/app/output.docx'
-      subprocess.run(['pandoc', '/app/input.html', '-f', 'html', '-t', 'docx', '-o', output_file])
+      reference_file = '/app/reference.docx'
+      subprocess.run(['pandoc', '/app/input.html', f'--reference-doc={reference_file}', '-f', 'html', '-t', 'docx', '-o', output_file])
 
       return send_file(output_file, as_attachment=True)
 
@@ -25,6 +26,7 @@ def convert_to_pdf():
     subprocess.run(['pandoc', '/app/input.html', '--standalone', '--listings', '--pdf-engine=lualatex', f'--template={template_file}', '--toc', '-f', 'html', '-t', 'pdf', '-o', output_file])
 
     return send_file(output_file, as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run()
