48412d8330adf0d3ecf8c415f855ae06155f52ad
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-04-11 19:01:40 +0300

    feat: Set "typescript" as default language for codeblocks

--- server/modules/rendering/html-codehighlighter/renderer.js
+++ server/modules/rendering/html-codehighlighter/renderer.js
@@ -4,7 +4,10 @@ module.exports = {
   async init($, config) {
     $('pre > code').each((idx, elm) => {
       const codeClasses = $(elm).attr('class') || ''
-      if (codeClasses.indexOf('language-') < 0) {
+
+      if (codeClasses === 'language-plaintext') {
+        $(elm).toggleClass('language-typescript')
+      } else if (codeClasses.indexOf('language-') < 0) {
         const result = hljs.highlightAuto($(elm).text())
         $(elm).addClass('language-', result.language)
       }
