40184f2af22d7443fda394b6eab7539dc8260b8e
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-04-11 12:55:01 +0300

    feat: Added "rainbow-braces" for prismjs

--- server/modules/rendering/html-codehighlighter/renderer.js
+++ server/modules/rendering/html-codehighlighter/renderer.js
@@ -7,9 +7,8 @@ module.exports = {
       if (codeClasses.indexOf('language-') < 0) {
         const result = hljs.highlightAuto($(elm).text())
         $(elm).addClass('language-', result.language)
-        $(elm).addClass('match-braces') // Ruslan: I don't sure if this line needed or not :)
       }
-      $(elm).parent().addClass('prismjs line-numbers match-braces')
+      $(elm).parent().addClass('prismjs line-numbers match-braces rainbow-braces')
     })
   }
 }
