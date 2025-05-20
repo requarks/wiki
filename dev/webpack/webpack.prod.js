d41b41bb491a12c19cfba657fe81592cf5a35d8c
Nils Urmersbach <nils.urmersbach@capgemini.com>
2025-05-20 15:57:46 +0200

    MAR-568: Fixing linting issues

--- dev/webpack/webpack.prod.js
+++ dev/webpack/webpack.prod.js
@@ -22,9 +22,9 @@ const babelConfig = fs.readJsonSync(path.join(process.cwd(), '.babelrc'))
 const cacheDir = '.webpack-cache/cache'
 const babelDir = path.join(process.cwd(), '.webpack-cache/babel')
 
-const { CKEditorTranslationsPlugin } = require( '@ckeditor/ckeditor5-dev-translations' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const { CKEditorTranslationsPlugin } = require('@ckeditor/ckeditor5-dev-translations')
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin')
+const { bundler, styles } = require('@ckeditor/ckeditor5-dev-utils')
 
 process.noDeprecation = true
 
@@ -74,29 +74,29 @@ module.exports = {
           {
             loader: 'style-loader',
             options: {
-                injectType: 'singletonStyleTag',
-                attributes: {
-                    'data-cke': true
-                }
+              injectType: 'singletonStyleTag',
+              attributes: {
+                'data-cke': true
+              }
             }
-        },
-        'css-loader',
-        {
+          },
+          'css-loader',
+          {
             loader: 'postcss-loader',
-            options: styles.getPostCssConfig( {
-                    themeImporter: {
-                        themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-                    },
-                    minify: true
-                } )
-        }
+            options: styles.getPostCssConfig({
+              themeImporter: {
+                themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+              },
+              minify: true
+            })
+          }
         ]
       },
       {
         test: /^(?!.*ckeditor).*\.css$/,
         exclude: [
           /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
-          path.join( __dirname, 'node_modules', '@ckeditor' ),
+          path.join(__dirname, 'node_modules', '@ckeditor')
         ],
         use: [
           'style-loader',
@@ -291,21 +291,21 @@ module.exports = {
       minChunkSize: 50000
     }),
     new CKEditorWebpackPlugin({
-			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-			// When changing the built-in language, remember to also change it in the editor's configuration (client/components/editor/ckeditor/ckeditor.js).
-			language: 'en',
-			additionalLanguages: 'all',
-      buildAllTranslationsToSeparateFiles: true,
-		}),
-		new webpack.BannerPlugin( {
-			banner: bundler.getLicenseBanner(),
-			raw: true
-		}),
-    new CKEditorTranslationsPlugin( {
+      // UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
+      // When changing the built-in language, remember to also change it in the editor's configuration (client/components/editor/ckeditor/ckeditor.js).
+      language: 'en',
+      additionalLanguages: 'all',
+      buildAllTranslationsToSeparateFiles: true
+    }),
+    new webpack.BannerPlugin({
+      banner: bundler.getLicenseBanner(),
+      raw: true
+    }),
+    new CKEditorTranslationsPlugin({
       // See https://ckeditor.com/docs/ckeditor5/latest/features/ui-language.html
       language: 'en',
-      buildAllTranslationsToSeparateFiles: true,
-  })
+      buildAllTranslationsToSeparateFiles: true
+    })
   ],
   optimization: {
     namedModules: true,
