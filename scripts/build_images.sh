1b85240d000bdf1515f5323245ff379a4f989a33
hari.gurudu <external.hari.gurudu@kuehne-nagel.com>
2025-08-07 06:15:00 +0200

    Revert changes like earilier git cicd

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -5,17 +5,12 @@ docker build --no-cache --build-arg VERSION="$VERSION" --build-arg RELEASE_DATE=
 echo "NEW_IMAGE=$NEW_IMAGE" >> newimage.txt
 # Conditionally build the Pandoc image if PANDOC is set to true
 
-
-if [[ "$PANDOC" == "true" ]]; then
-    echo "Building Pandoc Docker image..."
-    cd dev/pandoc
-    docker build --no-cache -f Dockerfile -t "$NEW_PANDOC_IMAGE" . || { echo "ERROR: Pandoc Docker image build failed!"; exit 1; }
-    cd ../..
-    echo "NEW_PANDOC_IMAGE=$NEW_PANDOC_IMAGE" >> newimage.txt
-    # Check if image exists locally
-    if ! docker image inspect "$NEW_PANDOC_IMAGE" > /dev/null 2>&1; then
-        echo "ERROR: Pandoc Docker image $NEW_PANDOC_IMAGE was not built!"; exit 1;
+    if [[ "$PANDOC" == "true" ]]; then
+        echo "Building Pandoc Docker image..."
+        cd dev/pandoc
+        docker build --no-cache -f Dockerfile -t "$NEW_PANDOC_IMAGE" .
+        cd ../..
+        echo "NEW_PANDOC_IMAGE=$NEW_PANDOC_IMAGE" >> newimage.txt
+        else
+        echo "Skipping Pandoc Docker image build as PANDOC is not set to true."
     fi
-else
-    echo "Skipping Pandoc Docker image build as PANDOC is not set to true."
-fi
--- scripts/set_up_runner.sh
+++ scripts/set_up_runner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export PATH="$NPM_GLOBAL_DIR/bin:$PATH"
-yarn cache clean --all
+yarn cache clean
 rm -rf node_modules
+rm -rf yarn.lock yarn.lock
 yarn install --frozen-lockfile --force
 yarn build
