3032de7bbb429e3fa6d7d4f36756b7b284fee7c7
kebba <kebba.kanyi@capgemini>
2025-10-12 10:15:09 +0200

    fixed Mermaid version compatability with webpacks

--- dev/webpack/webpack.dev-linux.js
+++ dev/webpack/webpack.dev-linux.js
@@ -49,7 +49,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            ...babelConfig,
+            cacheDirectory: babelDir
           }
         }
       },
