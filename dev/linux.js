acab71587c457cee26679b46057537d27f43ef27
kebba <kebba.kanyi@capgemini.com>
2025-10-26 17:46:57 +0100

    MAR-529-Update-webpack-dependencies

--- dev/linux.js
+++ dev/linux.js
@@ -20,43 +20,47 @@ const init = {
     global.DEV = true
     global.WP_CONFIG = require('./webpack/webpack.dev-linux.js')
     global.WP = webpack(global.WP_CONFIG)
+    const WebpackDevMiddleware = require('webpack-dev-middleware');
+    const WebpackHotMiddleware = require('webpack-hot-middleware');
     global.WP_DEV = {
-      devMiddleware: require('webpack-dev-middleware')(global.WP, {
+      devMiddleware: WebpackDevMiddleware(global.WP, {
         publicPath: global.WP_CONFIG.output.publicPath
       }),
-      hotMiddleware: require('webpack-hot-middleware')(global.WP)
+      hotMiddleware: WebpackHotMiddleware(global.WP)
+    };
+    if (typeof global.WP_DEV.devMiddleware.waitUntilValid === 'function') {
+      global.WP_DEV.devMiddleware.waitUntilValid(() => {
+        console.info(chalk.yellow.bold('>>> Starting Wiki.js in DEVELOPER mode...'));
+        require('../server');
+
+        process.stdin.setEncoding('utf8');
+        process.stdin.on('data', data => {
+          if (_.trim(data) === 'rs') {
+            console.warn(chalk.yellow.bold('--- >>>>>>>>>>>>>>>>>>>>>>>> ---'));
+            console.warn(chalk.yellow.bold('--- Manual restart requested ---'));
+            console.warn(chalk.yellow.bold('--- <<<<<<<<<<<<<<<<<<<<<<<< ---'));
+            this.reload();
+          }
+        });
+
+        const devWatcher = chokidar.watch([
+          './server',
+          '!./server/views/master.pug'
+        ], {
+          cwd: process.cwd(),
+          ignoreInitial: true,
+          atomic: 400
+        });
+        devWatcher.on('ready', () => {
+          devWatcher.on('all', _.debounce(() => {
+            console.warn(chalk.yellow.bold('--- >>>>>>>>>>>>>>>>>>>>>>>>>>>> ---'));
+            console.warn(chalk.yellow.bold('--- Changes detected: Restarting ---'));
+            console.warn(chalk.yellow.bold('--- <<<<<<<<<<<<<<<<<<<<<<<<<<<< ---'));
+            this.reload();
+          }, 500));
+        });
+      });
     }
-    global.WP_DEV.devMiddleware.waitUntilValid(() => {
-      console.info(chalk.yellow.bold('>>> Starting Wiki.js in DEVELOPER mode...'))
-      require('../server')
-
-      process.stdin.setEncoding('utf8')
-      process.stdin.on('data', data => {
-        if (_.trim(data) === 'rs') {
-          console.warn(chalk.yellow.bold('--- >>>>>>>>>>>>>>>>>>>>>>>> ---'))
-          console.warn(chalk.yellow.bold('--- Manual restart requested ---'))
-          console.warn(chalk.yellow.bold('--- <<<<<<<<<<<<<<<<<<<<<<<< ---'))
-          this.reload()
-        }
-      })
-
-      const devWatcher = chokidar.watch([
-        './server',
-        '!./server/views/master.pug'
-      ], {
-        cwd: process.cwd(),
-        ignoreInitial: true,
-        atomic: 400
-      })
-      devWatcher.on('ready', () => {
-        devWatcher.on('all', _.debounce(() => {
-          console.warn(chalk.yellow.bold('--- >>>>>>>>>>>>>>>>>>>>>>>>>>>> ---'))
-          console.warn(chalk.yellow.bold('--- Changes detected: Restarting ---'))
-          console.warn(chalk.yellow.bold('--- <<<<<<<<<<<<<<<<<<<<<<<<<<<< ---'))
-          this.reload()
-        }, 500))
-      })
-    })
   },
   async reload() {
     // handle that with the following
