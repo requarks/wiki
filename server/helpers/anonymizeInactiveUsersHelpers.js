940823e378e98636fa119e5b62fb78fd87657195
Przemek <przemyslaw.kowalczyk@capgemini.com>
2025-10-29 09:59:51 +0100

    Prevent cached pages from showing deleted usernames

--- server/helpers/anonymizeInactiveUsersHelpers.js
+++ server/helpers/anonymizeInactiveUsersHelpers.js
@@ -58,12 +58,28 @@ async function anonymizePageHistory(userSiteInactivity, anonymousUser, user) {
 }
 
 async function anonymizePages(userSiteInactivity, anonymousUser) {
+  // Get all pages where user was author or creator before updating
+  const affectedPages = await WIKI.models.pages.query()
+    .select('hash')
+    .where(builder => {
+      builder
+        .where({ authorId: userSiteInactivity.userId, siteId: userSiteInactivity.siteId })
+        .orWhere({ creatorId: userSiteInactivity.userId, siteId: userSiteInactivity.siteId })
+    })
+
+  // Update database records
   await WIKI.models.pages.query()
     .where({ authorId: userSiteInactivity.userId, siteId: userSiteInactivity.siteId })
     .patch({ authorId: anonymousUser.id })
   await WIKI.models.pages.query()
     .where({ creatorId: userSiteInactivity.userId, siteId: userSiteInactivity.siteId })
     .patch({ creatorId: anonymousUser.id })
+
+  // Invalidate cache for all affected pages to ensure updated user info is reflected
+  for (const page of affectedPages) {
+    await WIKI.models.pages.deletePageFromCache(page.hash)
+    WIKI.events.outbound.emit('deletePageFromCache', page.hash)
+  }
 }
 
 async function removeInactivityEntry(userSiteInactivity) {
--- server/models/users.js
+++ server/models/users.js
@@ -762,11 +762,26 @@ class User extends Model {
   static async deleteUser(id, replaceId) {
     const usr = await WIKI.models.users.query().findById(id)
     if (usr) {
+      // Get all pages where user was author or creator before updating (for cache invalidation)
+      const affectedPages = await WIKI.models.pages.query()
+        .select('hash')
+        .where(builder => {
+          builder
+            .where({ authorId: id })
+            .orWhere({ creatorId: id })
+        })
+
       await WIKI.models.assets.query().patch({ authorId: replaceId }).where('authorId', id)
       await WIKI.models.comments.query().patch({ authorId: replaceId }).where('authorId', id)
       await WIKI.models.pageHistory.query().patch({ authorId: replaceId }).where('authorId', id)
       await WIKI.models.pages.query().patch({ authorId: replaceId }).where('authorId', id)
       await WIKI.models.pages.query().patch({ creatorId: replaceId }).where('creatorId', id)
+
+      // Invalidate cache for all affected pages to ensure updated user info is reflected
+      for (const page of affectedPages) {
+        await WIKI.models.pages.deletePageFromCache(page.hash)
+        WIKI.events.outbound.emit('deletePageFromCache', page.hash)
+      }
 
       await WIKI.models.userKeys.query().delete().where('userId', id)
       await WIKI.models.users.query().deleteById(id)
