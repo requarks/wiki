d77d1f97bb96bc7c29a47bdc361deb7499a5a16d
Struzik <szymon.struzik@capgemini.com>
2026-02-26 15:02:45 +0100

    Created db migration for boolean column show_recent_site_activities

---
+++ server/db/migrations/2.5.151.js
@@ -0,0 +1,38 @@
+const { isPostgres } = require('./_helpers')
+
+exports.up = async function (knex) {
+  if (!isPostgres(knex)) {
+    return
+  }
+
+  await knex.transaction(async (trx) => {
+    const hasColumn = await trx.schema.hasColumn('sites', 'show_recent_activities')
+    if (!hasColumn) {
+      await knex.schema.alterTable('sites', (table) => {
+        table.boolean('show_recent_activities').defaultTo(false)
+      })
+
+      // Safety: ensure any NULLs (if present) are set to false
+      await trx.raw(`
+        UPDATE sites
+        SET show_recent_activities = false
+        WHERE show_recent_activities IS NULL
+      `)
+    }
+  })
+}
+
+exports.down = async function (knex) {
+  if (!isPostgres(knex)) {
+    return
+  }
+
+  await knex.transaction(async (trx) => {
+    const hasColumn = await trx.schema.hasColumn('sites', 'show_recent_activities')
+    if (hasColumn) {
+      await trx.schema.table('sites', (table) => {
+        table.dropColumn('show_recent_activities')
+      })
+    }
+  })
+}
